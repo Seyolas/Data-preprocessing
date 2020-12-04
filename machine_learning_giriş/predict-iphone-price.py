2e7e7c78ce184213ab5774f374ec2c39c3efb4f9
Seyolas <39236950+Seyolas@users.noreply.github.com>
2020-12-04 23:35:12 +0300

    introduction

---
+++ machine_learning_giriş/predict-iphone-price.py
@@ -0,0 +1,10 @@
+import pandas
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('iphone_price.csv')
+plt.scatter(data['version'], data['price'])
+plt.show()
+model = LinearRegression()
+model.fit(data[['version']], data[['price']])
+print(model.predict([[30]]))
